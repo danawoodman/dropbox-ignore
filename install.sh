1d8c55c3c0454c2053a100a9ce77eb8a39fbc778
Dana Woodman <dana@danawoodman.com>
2020-04-28 12:54:27 -0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+# Move to directory on PATH
+cd /usr/local/bin
+
+# Download script
+curl -LJO https://raw.githubusercontent.com/danawoodman/dropbox-ignore/master/dropbox-ignore
+
+# Make executable
+chmod +x dropbox-ignore
+
+echo
+echo "✅ Install successful, you can now run 'dropbox-ignore'."
+echo
